10f3d18ec285670320e10c52dd6afe56d62649b5
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-03-18 23:11:38 +0100

    fix: use feather x icon

--- src/components/ConferencesSearch.js
+++ src/components/ConferencesSearch.js
@@ -59,15 +59,7 @@ const ConferencesSearch = ({ onSearch }) => {
       />
       {query.length > 0 && (
         <ClearButton type="button" onClick={clearHandler}>
-          <svg
-            enableBackground={'new 0 0 386.667 386.667'}
-            height="10"
-            viewBox="0 0 386.667 386.667"
-            width="10"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="m386.667 45.564-45.564-45.564-147.77 147.769-147.769-147.769-45.564 45.564 147.769 147.769-147.769 147.77 45.564 45.564 147.769-147.769 147.769 147.769 45.564-45.564-147.768-147.77z" />
-          </svg>
+          <i data-feather="x"></i>
         </ClearButton>
       )}
     </Form>
