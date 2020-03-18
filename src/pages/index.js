e8a16e9ddba741f95e739b30756f658c1f47d372
Nuno <n.alexandre@xgeeks.io>
2020-03-18 14:17:19 +0000

    feat(search bar): add search bar field and functionality
    Closes #5

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,39 +7,73 @@ import Container from '../components/Container'
 import { useSiteMetadata } from '../hooks/use-site-metadata'
 import Intro from '../components/Intro'
 import ConferencesList from '../components/ConferencesList'
+import ConferencesSearch from '../components/ConferencesSearch'
+import EmptyListText from '../components/EmptyListText'
 
 const URL = 'https://homeferences.github.io/list/homeferences.json'
 
 const IndexPage = () => {
   const [conferences, setConferences] = useState([])
+  const [loading, setLoading] = useState(true)
   const { intro } = useSiteMetadata()
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(URL)
-        let data = await response.json()
+  const fetchData = async (query = '') => {
+    try {
+      setLoading(true)
+      const response = await fetch(URL)
+      let data = await response.json()
 
-        setConferences(data)
-      } catch (e) {
-        console.log('error', e)
-      }
+      setConferences(
+        data.filter(conference => {
+          let inDescription = conference.description
+            ? conference.description
+                .toLowerCase()
+                .indexOf(query.toLowerCase()) >= 0
+            : false
+          let inTopic = conference.topic
+            ? conference.topic.toLowerCase().indexOf(query.toLowerCase()) >= 0
+            : false
+          let inTitle = conference.name
+            ? conference.name.toLowerCase().indexOf(query.toLowerCase()) >= 0
+            : false
+          return inDescription || inTopic || inTitle
+        })
+      )
+      setLoading(false)
+    } catch (e) {
+      console.log('error', e)
+      setLoading(false)
     }
+  }
 
+  useEffect(() => {
     fetchData()
   }, [])
+
+  const onSearch = query => {
+    setConferences([])
+    fetchData(query)
+  }
 
   return (
     <Layout>
       <SEO />
       <Container fullWidth noPadding>
         <Intro text={intro} />
-        {conferences.length > 0 && (
+        <ConferencesSearch onSearch={onSearch} />
+        {!loading && conferences.length > 0 && (
           <ConferencesList conferences={conferences} />
         )}
-        {conferences.length === 0 && (
+
+        {loading && conferences.length === 0 && (
           <div style={{ width: '100hw', textAlign: 'center' }}>
             <Spinner />
+          </div>
+        )}
+
+        {!loading && conferences.length === 0 && (
+          <div style={{ width: '100hw', textAlign: 'center' }}>
+            <EmptyListText />
           </div>
         )}
       </Container>
